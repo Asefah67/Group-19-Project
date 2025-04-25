976b22031ccf580c2965c873f748ba7085c5a5d7
Ama Sefah <amasefah67@gmail.com>
2025-04-25 17:09:41 -0400

    updates

--- chat-room/createGC.js
+++ chat-room/createGC.js
@@ -1,8 +1,30 @@
-<iframe id="frame" src="create-group\create-group.html"></iframe>
+window.onload = () => {
+    const elements = document.querySelector(".conversation-list");
+    console.log("Elements found:", elements);
+};
 
-const iframe = document.getElementById('frame');
-
-iframe.onload = () => {
-    const button = iframe.contentWindow.document.getElementById('create_group');
-    
+export function createGC () {
+    fetch("../server/data/rooms/groups.json")
+    .then(res => res.json())
+    .then(data => {
+        for (const key in data) {
+            console.log(data)
+            if (key === 'groups') {
+                data[key].forEach(obj => {
+                    console.log(JSON.stringify(obj))
+                    if (obj.status === false) {
+                        setTimeout(() => {
+                            const element = document.getElementsByClassName("conversation-list")
+                            const new_chat = document.createElement('div');
+                            new_chat.classList.add('conversation');
+                            element.appendChild(new_chat);
+                            console.log("Done")
+                        }, 1000)
+                    }
+                })
+            }
+        }
+    })
+    console.log("Complete")
 }
+window.createGC = createGC;
---
+++ server/services/Server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 5501;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('server\data\rooms\groups.json', (req, res) => {
+    const jsonPath = path.join(__dirname, 'data', 'groups.json');
+    const jsonData = fs.readFileSync(jsonPath, 'utf8');
+    res.json(JSON.parse(jsonData));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://127.0.0.1:5501`)
+})
